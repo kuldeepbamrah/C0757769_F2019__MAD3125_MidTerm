3a7587727f32eb6c74668876ca97f8716d8fe093
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-12-01 22:15:15 -0500

    adding layout for customer details

--- app/src/main/java/com/example/c0757769_f2019_mad3125_midterm/Activities/CustomerDetailActivity.java
+++ app/src/main/java/com/example/c0757769_f2019_mad3125_midterm/Activities/CustomerDetailActivity.java
@@ -1,12 +1,18 @@
 package com.example.c0757769_f2019_mad3125_midterm.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.c0757769_f2019_mad3125_midterm.R;
 
-public class CustomerDetailActivity extends AppCompatActivity {
+public class CustomerDetailActivity extends AppCompatActivity
+{
+    TextView custid, custname, custemail, custage;
+    RecyclerView myrecycler;
+    TextView detailtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
