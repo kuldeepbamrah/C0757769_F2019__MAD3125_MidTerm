fb13dd70a2e69c60eb99bcec5b48f72a373d4765
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-12-01 22:23:42 -0500

    populating bill recycler view

--- app/src/main/java/com/example/c0757769_f2019_mad3125_midterm/Activities/CustomerDetailActivity.java
+++ app/src/main/java/com/example/c0757769_f2019_mad3125_midterm/Activities/CustomerDetailActivity.java
@@ -1,15 +1,20 @@
 package com.example.c0757769_f2019_mad3125_midterm.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.c0757769_f2019_mad3125_midterm.Adapters.BillAdapter;
+import com.example.c0757769_f2019_mad3125_midterm.ModelClasses.Bill;
+import com.example.c0757769_f2019_mad3125_midterm.ModelClasses.Customer;
 import com.example.c0757769_f2019_mad3125_midterm.R;
 
-public class CustomerDetailActivity extends AppCompatActivity
-{
+import java.util.List;
+
+public class CustomerDetailActivity extends AppCompatActivity {
     TextView custid, custname, custemail, custage;
     RecyclerView myrecycler;
     TextView detailtext;
@@ -18,5 +23,45 @@ public class CustomerDetailActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_detail);
+
+        Customer custtemp = getIntent().getParcelableExtra("custobject");
+
+
+        custid = findViewById(R.id.textEmpID);
+        custname = findViewById(R.id.textEmpName);
+        custage = findViewById(R.id.textEmpAge);
+        custemail = findViewById(R.id.textEmpEmail);
+        detailtext = findViewById(R.id.detailtext1);
+
+        String empid = String.valueOf(custtemp.getId());
+        custid.setText(empid);
+
+        String fname = custtemp.getFirstName() + " " + custtemp.getLastName();
+
+        custname.setText(fname);
+
+        String empage = String.valueOf(custtemp.getAge()) + " years";
+
+        custage.setText(empage);
+        custemail.setText(custtemp.getEmail());
+
+
+        List<Bill> myBillList = custtemp.getBill();
+
+        if (myBillList == null) {
+            detailtext.setText("Customer has no Bills to pay");
+        } else {
+
+            myrecycler = (RecyclerView) findViewById(R.id.recycler_vehicle);
+
+            final BillAdapter myadapter = new BillAdapter(this);
+            myadapter.notifyDataSetChanged();
+            myadapter.setMyaaraylist(myBillList);
+            myadapter.notifyDataSetChanged();
+
+            LinearLayoutManager mylinearlayout = new LinearLayoutManager(this);
+            myrecycler.setLayoutManager(mylinearlayout);
+            myrecycler.setAdapter(myadapter);
+        }
     }
 }
---
+++ app/src/main/java/com/example/c0757769_f2019_mad3125_midterm/Adapters/BillAdapter.java
@@ -0,0 +1,111 @@
+package com.example.c0757769_f2019_mad3125_midterm.Adapters;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.c0757769_f2019_mad3125_midterm.ModelClasses.Bill;
+import com.example.c0757769_f2019_mad3125_midterm.R;
+
+import java.util.List;
+
+
+public class BillAdapter extends RecyclerView.Adapter<BillAdapter.ViewHolder> {
+
+    private Activity context;
+    private List<Bill> myaaraylist;
+
+
+    public BillAdapter(Activity context) {
+        this.context = context;
+
+    }
+
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Activity context) {
+        this.context = context;
+    }
+
+    public List<Bill> getMyaaraylist() {
+        return myaaraylist;
+    }
+
+    public void setMyaaraylist(List<Bill> myaaraylist) {
+        this.myaaraylist = myaaraylist;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bill_cell, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+
+        final Bill mydata = myaaraylist.get(position);
+
+        final String billidtemp = String.valueOf(mydata.getBillId());
+
+        holder.billid.setText("Bill ID: " + billidtemp);
+        holder.billtype.setText("Bill Type: " + mydata.getBillType());
+        holder.billdate.setText("Bill Date: " + mydata.getBillDate());
+        holder.billamount.setText("Bill Amount: " + mydata.getBillAmount() + " $");
+
+
+        holder.mycardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String alldatavehicle = "Bill ID: " + billidtemp + "\n" + "Bill Type: " + mydata.getBillType() + "\n" + "Bill Date: " + mydata.getBillDate() + "\n" + "Bill Amount: " + mydata.getBillAmount() + " $";
+                //  Toast.makeText(context,"position = "+position,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return myaaraylist.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView billid, billdate, billtype, billamount;
+        CardView mycardview;
+
+        public ViewHolder(@NonNull View itemView) {
+
+
+            super(itemView);
+
+            mycardview = itemView.findViewById(R.id.newcard);
+            billid = itemView.findViewById(R.id.textView1);
+            billtype = (TextView) itemView.findViewById(R.id.textView2);
+            billdate = (TextView) itemView.findViewById(R.id.textView3);
+            billamount = (TextView) itemView.findViewById(R.id.textView4);
+
+
+        }
+    }
+
+
+
+}
