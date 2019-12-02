e5463464a3a1b72fee657e2c883e323678fa47a3
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-12-01 20:46:02 -0500

    Adding UI elements

--- app/src/main/java/com/example/c0757769_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0757769_f2019_mad3125_midterm/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.c0757769_f2019_mad3125_midterm;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+ActionBar actionBar;
+EditText email,password;
+TextView login_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        actionBar = getSupportActionBar();
+        actionBar.hide();
 
+        email = findViewById()
 
 
 
